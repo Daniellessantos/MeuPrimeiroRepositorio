65f99d4141c43d2fdb03238008b797d78d69042a
DanielleSquilino <dani.squilino@gmail.com>
2024-07-19 09:37:03 -0300

    add um php com loop ou laço

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $x = 4;
+    while ($x <=100){
+        echo "o número é: $x <br>";
+        $x+=10;
+    }
+
+    ?>
+</body>
+</html>
